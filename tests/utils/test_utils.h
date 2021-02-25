f2ef39625f3af7a7cc2db253e7feb8e5cc717dcc
Simiyutin Boris <simiyutin.boris@yandex.ru>
2021-02-26 00:54:53 +0300

    Task 03 implemented

--- tests/utils/test_utils.h
+++ tests/utils/test_utils.h
@@ -7,3 +7,10 @@ cv::Mat concatenateImagesLeftRight(const cv::Mat &img0, const cv::Mat &img1);
 std::string getTestName();
 
 std::string getTestSuiteName();
+
+void drawMatches(const cv::Mat &img1,
+                 const cv::Mat &img2,
+                 const std::vector<cv::KeyPoint> &keypoints1,
+                 const std::vector<cv::KeyPoint> &keypoints2,
+                 const std::vector<cv::DMatch> &matches,
+                 const std::string &path);
