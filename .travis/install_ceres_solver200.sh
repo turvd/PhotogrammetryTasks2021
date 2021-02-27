26aeea9a489ce24cdea00f7618eec67fa189eed0
PolarNick <polarnick239@gmail.com>
2021-02-27 19:53:35 +0300

    ceres: instruction for Linux fixed

--- .travis/install_ceres_solver200.sh
+++ .travis/install_ceres_solver200.sh
@@ -4,6 +4,8 @@ sudo apt install libgoogle-glog-dev libgflags-dev libatlas-base-dev libeigen3-de
 wget https://github.com/ceres-solver/ceres-solver/archive/2.0.0.zip
 unzip 2.0.0.zip
 cd ceres-solver-2.0.0
+mkdir build
+cd build
 cmake -DCMAKE_INSTALL_PREFIX=/opt/ceres-solver200 ..
 make -j12
 sudo make install
