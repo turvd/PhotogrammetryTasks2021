bcd478629135e9f638c5eab0c02655c993f74130
PolarNick <polarnick239@gmail.com>
2021-02-27 18:41:06 +0300

    ceres-solver experiments

---
+++ .travis/install_ceres_solver200.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt install libgoogle-glog-dev libgflags-dev libatlas-base-dev libeigen3-dev libsuitesparse-dev
+wget https://github.com/ceres-solver/ceres-solver/archive/2.0.0.zip
+unzip 2.0.0.zip
+cd ceres-solver-2.0.0
+cmake -DCMAKE_INSTALL_PREFIX=/opt/ceres-solver200 ..
+make -j12
+sudo make install
